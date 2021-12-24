45af19b1f1032a6c5be949a4c2bcb58c6fe8040c
Cengizhan <cengizhanyurdakul97@gmail.com>
2021-12-24 18:02:56 +0300

    Face recognition model added

---
+++ src/recognition/FaceRecognition.py
@@ -0,0 +1,17 @@
+import torch
+
+from src.recognition.backbone import IResNet
+
+class FaceRecognizer:
+    def __init__(self, **args):
+        self.args = args
+
+        self.faceRecognitionModel = IResNet()
+        self.faceRecognitionModel.load_state_dict(torch.load(self.args["weightPath"]))
+        self.faceRecognitionModel.eval()
+        self.faceRecognitionModel.to(self.args["device"])
+
+        print("Face recognition model weights initialized!")
+
+    def main(self):
+        pass
