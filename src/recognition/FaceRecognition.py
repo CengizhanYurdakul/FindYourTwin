a4ccc261b23f320071bcdc0d0cee603ebed19c12
Cengizhan <cengizhanyurdakul97@gmail.com>
2021-12-24 21:48:16 +0300

    Preprocess and identity extraction added

--- src/recognition/FaceRecognition.py
+++ src/recognition/FaceRecognition.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 from src.recognition.backbone import IResNet
 
@@ -12,9 +13,32 @@ class FaceRecognizer:
         self.faceRecognitionModel.to(self.args["device"])
 
         print("Face recognition model weights initialized!")
+        
+    def preprocess(self, inputImage:np.array)->torch.Tensor:
+        """
+        It preprocesses the array, which will enter the face recognition model, such as permute and normalization.
 
-    def extractIdentity(self, inputImage):
-        pass
+        Args:
+            inputImage (np.array): The image to be preprocessed.
 
-    def main(self):
-        pass
+        Returns:
+            torch.Tensor: A preprocessed tensor.
+        """
+        inputImage = np.transpose(inputImage, (2, 0, 1))
+        inputImage = torch.from_numpy(inputImage).unsqueeze(0).float()
+        inputImage.div_(255).sub_(0.5).div_(0.5)
+        return inputImage.to(self.args["device"])
+
+    def extractIdentity(self, inputImage:np.array)->torch.Tensor:
+        """
+        It gives the input to the face recognition model and takes the identity output.
+
+        Args:
+            inputImage (np.array): The image whose identity is desired to be obtained.
+
+        Returns:
+            torch.Tensor: Identity
+        """
+        inputImage = self.preprocess(inputImage)
+        inputIdentity = self.faceRecognitionModel(inputImage)
+        return inputIdentity
