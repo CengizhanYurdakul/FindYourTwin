6eea539fc6b6fd53fc9f0b5dc21b0e4e13027db4
Cengizhan <cengizhanyurdakul97@gmail.com>
2021-12-24 21:13:02 +0300

    Detection results parser with respect to one face which is determined from its area. Also new bounding box coordinate formats which are xyxy and xywh added

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -23,4 +23,52 @@ def parseDetectionResult(allInformations:np.array)->np.array:
     Returns:
         np.array: Bounding box and 5 landmark information.
     """
-    return allInformations[0], allInformations[2]
+    boundingBoxes = allInformations[0]
+    landmark5 = allInformations[2]
+    index = findBiggestFace(boundingBoxes)
+    return boundingBoxes[index], landmark5[index]
+
+def findBiggestFace(boundingBoxes:np.array)->int:
+    """
+    It takes the largest face among the detected images of more than one face.
+
+    Args:
+        boundingBoxes (np.array): Coordinates of detected faces.
+
+    Returns:
+        int: The index with the largest face.
+    """
+    biggestIndex = None
+    biggestArea = 0
+    for c, i in enumerate(boundingBoxes):
+        boundingBox = parseBoundingBox(i)
+        area = boundingBox[2] * boundingBox[3]
+        if area > biggestArea:
+            biggestArea = area
+            biggestIndex = c
+    return biggestIndex
+    
+def parseBoundingBox(boundingBox:np.array, type:str="xywh")->np.array:
+    """
+    Converting the coordinates of the points where the faces are located to the formats "xyxy" and "xywh" to use different types.
+
+    Args:
+        boundingBox (np.array): Coordinates of detected faces.
+        type (str, optional): In the coordinates of the position of the face, "xyxy" gives the x,y coordinates of the upper left 
+        point and the x,y coordinates of the lower right point, and "xywh" gives the x,y coordinates of the upper left point, its 
+        height and width.. Defaults to "xywh".
+
+    Raises:
+        Exception: If a format other than these two formats is entered.
+
+    Returns:
+        np.array: An array of bounding boxes in "xywh" or "xyxy" format.
+    """
+    if type == "xyxy":
+        parsedBoundingBox = [boundingBox[0], boundingBox[1], boundingBox[2], boundingBox[3]]
+    elif type == "xywh":
+        parsedBoundingBox = [boundingBox[0], boundingBox[1], boundingBox[2] - boundingBox[0], boundingBox[3] - boundingBox[1]]
+    else:
+        raise Exception("Unknown type of parse bounding box!")
+    
+    return np.array(parsedBoundingBox)
