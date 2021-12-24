3a84f0eb350b2bd0d42b6b1ca64afc2e45448836
Cengizhan <cengizhanyurdakul97@gmail.com>
2021-12-24 20:10:19 +0300

    Face detector added

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import argparse
 
+from src.detection.FaceDetection import FaceDetector
 from src.recognition.FaceRecognition import FaceRecognizer
+
 
 
 class CelebrityFinder:
     def __init__(self, **args):
         self.recognizer = FaceRecognizer(**args)
-        #TODO add face recognition model
+        self.detector =  FaceDetector()
         #TODO add identity pool
         #TODO add cosine similarity
+        #TODO add face alignment
+
         pass
 
     def main(self):
