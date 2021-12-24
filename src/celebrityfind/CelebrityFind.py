fa64711f08971525221cdbeee6a52f575e7dd8b9
Cengizhan <cengizhanyurdakul97@gmail.com>
2021-12-25 02:03:16 +0300

    Run clearly but need debug and methods needs to be seperated

--- src/celebrityfind/CelebrityFind.py
+++ src/celebrityfind/CelebrityFind.py
@@ -1,20 +1,54 @@
+import os
 import cv2
-import argparse
+import pickle
+import numpy as np
+from tqdm import tqdm
+from sklearn.metrics.pairwise import cosine_similarity 
 
 from src.detection.FaceDetection import FaceDetector
 from src.recognition.FaceRecognition import FaceRecognizer
 
 class CelebrityFinder:
     def __init__(self, **args):
-        self.recognizer = FaceRecognizer(**args)
-        self.detector =  FaceDetector()
-        #TODO add identity pool
-        #TODO add cosine similarity
-        pass
-
-    def main(self):
-        img = cv2.cvtColor(cv2.imread("test1.jpg"), cv2.COLOR_BGR2RGB)
-        aligned = self.detector.alignFace(img)
-        identity = self.recognizer.extractIdentity(aligned)
-        pass
-
+        self.args = args
+        
+        self.faceDetector = FaceDetector(**self.args)        
+        self.faceRecognizer = FaceRecognizer(**self.args)
+        
+        with open(self.args["poolResultName"], "rb") as f:
+            self.identityPool = pickle.load(f)
+        print("Identity pickle loaded!")
+        
+        self.maxSimilarityImageName = None
+        self.maxSimilarity = -1
+        
+    def visualizeCelebrityVersion(self, inputImage):
+        celebrityImage = cv2.imread(os.path.join(self.args["imagePaths"], self.maxSimilarityImageName))
+        inputImage = cv2.cvtColor(inputImage, cv2.COLOR_RGB2BGR)
+        
+        inputImage[:int(inputImage.shape[1]/4), int(3*inputImage.shape[1]/4):] = cv2.resize(celebrityImage, (int(inputImage.shape[1]/4), int(inputImage.shape[1]/4)))
+        
+        
+        
+        cv2.imwrite("Celebrity.jpg", inputImage)
+        print("Your similarity with your celebrity: %s" % round(self.maxSimilarity, 3))
+ 
+    def find(self):
+        inputImage = cv2.imread(self.args["yourImage"])
+        inputImage = cv2.cvtColor(inputImage, cv2.COLOR_BGR2RGB)
+        
+        alignedImage = self.faceDetector.alignFace(inputImage)
+        
+        if alignedImage is None:
+            raise Exception("No face detected!")
+        
+        inputIdentity = self.faceRecognizer.extractIdentity(alignedImage)
+        
+        for celebrityImageName in tqdm(self.identityPool.keys()):
+            celebrityIdentity = self.identityPool[celebrityImageName]
+            cosineSimilarity = float(cosine_similarity(inputIdentity.cpu(), celebrityIdentity.cpu()))
+            if cosineSimilarity > self.maxSimilarity:
+                self.maxSimilarity = cosineSimilarity
+                self.maxSimilarityImageName = celebrityImageName
+        
+        self.visualizeCelebrityVersion(inputImage)
